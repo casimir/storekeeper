6a4267bed83c23bd2bdd0e82bc422f883862f5d6
Martin Chaine <chaine.martin@gmail.com>
2014-05-05 20:38:58 +0200

    Follow OS naming convention

--- util/misc.go
+++ util/misc.go
@@ -8,11 +8,11 @@ import (
 func ApplicationPath() string {
 	switch runtime.GOOS {
 	case "darwin":
-		return os.Getenv("HOME") + "/Library/Application Support/storekeeper"
+		return os.Getenv("HOME") + "/Library/Application Support/Storekeeper"
 	case "linux":
 		return os.Getenv("HOME") + "/.storekeeper"
 	case "windows":
-		return os.Getenv("APPDATA") + "/storekeeper"
+		return os.Getenv("APPDATA") + "/Storekeeper"
 	default:
 		return os.Getenv("TMP") + "/storekeeper"
 	}
