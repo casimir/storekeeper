6dca2b9d1e264a8f93d1ee2dc6a9acbe89210698
Martin Chaine <chaine.martin@gmail.com>
2014-05-26 22:23:13 +0200

    Fix test (goconvey update)

--- util/stringset_test.go
+++ util/stringset_test.go
@@ -1,26 +1,29 @@
 package util
 
 import (
-	. "github.com/smartystreets/goconvey/convey"
 	"testing"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestAdd(t *testing.T) {
 	Convey("Given several string", t, func() {
-		set := StringSet{}
-		So(set.Len(), ShouldBeZeroValue)
-		Convey("It should add it to the set", func() {
-			So(set.Add("some string"), ShouldBeTrue)
-			So(set.Add("another string"), ShouldBeTrue)
-			So(set.Add("one last string"), ShouldBeTrue)
-			So(set.Add("zorro string"), ShouldBeTrue)
+		set := &StringSet{}
+		l := []string{
+			"some string",
+			"another string",
+			"one last string",
+			"zorro string",
+		}
+
+		Convey("It should add it to the set if not present", func() {
+			So(set.Len(), ShouldBeZeroValue)
+			for _, it := range l {
+				So(set.Add(it), ShouldBeTrue)
+			}
 			So(set.Len(), ShouldEqual, 4)
-		})
-		Convey("It should not add it to the set if already in", func() {
-			So(set.Add("some string"), ShouldBeFalse)
-			So(set.Add("another string"), ShouldBeFalse)
-			So(set.Add("one last string"), ShouldBeFalse)
-			So(set.Add("zorro string"), ShouldBeFalse)
+			for _, it := range l {
+				So(set.Add(it), ShouldBeFalse)
+			}
 			So(set.Len(), ShouldEqual, 4)
 		})
 	})
