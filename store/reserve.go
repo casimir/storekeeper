97c0e3ab5da4800d02602ce2b4ce7ffebdc73c48
Martin Chaine <chaine.martin@gmail.com>
2015-01-16 01:27:50 +0100

    db or NOT db?

--- store/reserve.go
+++ store/reserve.go
@@ -1,129 +1,53 @@
 package store
 
 import (
-	"database/sql"
 	"encoding/json"
-	"errors"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 
-	"github.com/casimir/storekeeper/kitchen"
-	"github.com/casimir/storekeeper/storage"
 	"github.com/casimir/storekeeper/util"
-	"github.com/coopernurse/gorp"
-	_ "github.com/mattn/go-sqlite3" // SQlite connection driver
 )
 
-func init() {
-	os.MkdirAll(dbDir, 0755)
-}
-
-var dbDir = util.ApplicationPath() + "/db"
+var dbDir = util.ApplicationPath() + "/data"
 
 func dbPath(name string) string {
-	return dbDir + "/" + name + ".db"
+	return fmt.Sprintf("%s/%s.json", dbDir, name)
 }
 
-// Reserve provides a way to persist a store.Store.
+// Reserve provides a way to persist a Store.
 type Reserve struct {
-	dbm *gorp.DbMap
+	name string
 }
 
 // NewReserve create a reserve with the given name.
-func NewReserve(storeName string) *Reserve {
-	db, err := sql.Open("sqlite3", dbPath(storeName))
-	if err != nil {
-		panic(err)
-	}
-	ret := &Reserve{
-		&gorp.DbMap{
-			Db:            db,
-			Dialect:       gorp.SqliteDialect{},
-			TypeConverter: converter{},
-		},
-	}
-	if ret.init() != nil {
-		panic(err)
-	}
-	return ret
-}
-
-func (r *Reserve) init() error {
-	r.dbm.AddTableWithName(Artisan{}, "artisans").SetKeys(false, "ID")
-	r.dbm.AddTableWithName(storage.Item{}, "items").SetKeys(false, "ID")
-	r.dbm.AddTableWithName(kitchen.Recipe{}, "recipes").SetKeys(false, "ID")
-	return r.dbm.CreateTablesIfNotExists()
+func NewReserve(name string) *Reserve {
+	os.MkdirAll(dbDir, 0755)
+	return &Reserve{name: name}
 }
 
 // Load the Store from the Reserve.
-func (r Reserve) Load() *Store {
-	s := &Store{}
-	_, err := r.dbm.Select(&s.Artisans, "select * from artisans")
+func (r *Reserve) Load() *Store {
+	ret := new(Store)
+	data, err := ioutil.ReadFile(dbPath(r.name))
 	if err != nil {
-		log.Printf("Failed to load artisans: %s\n", err)
-		err = nil
+		log.Printf("Failed to load store %s: %s\n", r.name, err)
+		return &Store{}
 	}
-	_, err = r.dbm.Select(&s.Book, "select * from recipes")
-	if err != nil {
-		log.Printf("Failed to load recipes: %s\n", err)
-		err = nil
-	}
-	_, err = r.dbm.Select(&s.Catalog, "select * from items")
-	if err != nil {
-		log.Printf("Failed to load items: %s\n", err)
-		err = nil
-	}
-	return s
+	json.Unmarshal(data, &ret)
+	return ret
 }
 
 // Save the store to the Reserve.
-func (r Reserve) Save(s *Store) {
-	for _, a := range s.Artisans {
-		if err := r.dbm.Insert(&a); err != nil {
-			log.Printf("Error while saving artisans: %s", err.Error())
-		}
+func (r *Reserve) Save(s *Store) error {
+	data, _ := json.Marshal(s)
+	if err := ioutil.WriteFile(dbPath(r.name), data, 0644); err != nil {
+		return fmt.Errorf("Failed to set %s: %s", r.name, err)
 	}
-	for _, b := range s.Book {
-		if err := r.dbm.Insert(&b); err != nil {
-			log.Printf("Error while saving recipes: %s", err.Error())
-		}
-	}
-	for _, c := range s.Catalog {
-		if err := r.dbm.Insert(&c); err != nil {
-			log.Printf("Error while saving catalog: %s", err.Error())
-		}
-	}
+	return nil
 }
 
 // DeleteReserve delete a persisted store. Does nothing if the store does not
 // exist.
-func DeleteReserve(storeName string) { os.Remove(dbPath(storeName)) }
-
-type converter struct{}
-
-func (c converter) ToDb(val interface{}) (interface{}, error) {
-	switch v := val.(type) {
-	case storage.Stack, []storage.Stack:
-		raw, err := json.Marshal(v)
-		if err != nil {
-			return "", err
-		}
-		return string(raw), nil
-	}
-	return val, nil
-}
-
-func (c converter) FromDb(target interface{}) (gorp.CustomScanner, bool) {
-	switch target.(type) {
-	case *storage.Stack, *[]storage.Stack:
-		binder := func(holder, target interface{}) error {
-			s, ok := holder.(*string)
-			if !ok {
-				return errors.New("Error while loading Stack type")
-			}
-			return json.Unmarshal([]byte(*s), target)
-		}
-		return gorp.CustomScanner{new(string), target, binder}, true
-	}
-	return gorp.CustomScanner{}, false
-}
+func DeleteReserve(name string) { os.Remove(dbPath(name)) }
--- store/reserve_test.go
+++ store/reserve_test.go
@@ -4,97 +4,38 @@ import (
 	"os"
 	"testing"
 
-	"github.com/casimir/storekeeper/storage"
-	"github.com/casimir/storekeeper/util"
-	"github.com/coopernurse/gorp"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestReservePath(t *testing.T) {
-	Convey("Given a store name", t, func() {
-		Convey("It should return the DB path", func() {
-			// Bad coupling but it's an utility package after all
-			So(dbPath("name"), ShouldEqual, util.ApplicationPath()+"/db/name.db")
-		})
-	})
-}
-
-func TestReserveLifecycle(t *testing.T) {
+func TestStoreSaveAndLoad(t *testing.T) {
+	dbDir = "/tmp/data"
 	name := "test"
-	Convey("Given a store name", t, func() {
-		Convey("It should be able to create the reserve", func() {
-			r := NewReserve(name)
-			So(fileExists(dbPath(name)), ShouldBeTrue)
-			r.dbm.Db.Close()
-		})
-		Convey("It should be able to delete the reserve", func() {
-			DeleteReserve(name)
-			So(fileExists(dbPath(name)), ShouldBeFalse)
-		})
+	r1 := NewReserve(name)
+	r2 := NewReserve(name)
+
+	Convey("A Store should be savable and loadable.", t, func() {
+		So(r1.Save(newMockStore()), ShouldBeNil)
+		So(fileExists(dbPath(name)), ShouldBeTrue)
+		So(r2.Load(), ShouldResemble, newMockStore())
+		DeleteReserve(name)
+		So(fileExists(dbPath(name)), ShouldBeFalse)
+	})
+	Convey("Reserve should handle invalid states", t, func() {
+		os.Remove(dbDir)
+		So(r1.Save(newMockStore()), ShouldNotBeNil)
+		So(r1.Load(), ShouldResemble, &Store{})
 	})
 }
 
-func TestStoreSAveAndLoad(t *testing.T) {
-	// TODO mockup fake store to test transactions
-}
-
-func TestDBConversion(t *testing.T) {
-	jsonEmpty := "[]"
-	sliceEmpty := []storage.Stack{}
-	json := "[{\"Count\":1,\"Item\":{\"ID\":\"id1\",\"Name\":\"id1\"}},{\"Count\":2,\"Item\":{\"ID\":\"id2\",\"Name\":\"id2\"}}]"
-	slice := []storage.Stack{{1, storage.Item{"id1", "id1"}}, {2, storage.Item{"id2", "id2"}}}
-
-	resEmpty := stackToDb(sliceEmpty)
-	res := stackToDb(slice)
-
-	Convey("Given a slice of Stacks", t, func() {
-		Convey("It should serialize the slice to store in DB", func() {
-			So(typeToDb(1), ShouldEqual, 1)
-			So(resEmpty, ShouldEqual, jsonEmpty)
-			So(res, ShouldEqual, json)
-		})
-		Convey("It should deserialize the value in DB to a slice", func() {
-			So(dbToType(1), ShouldResemble, gorp.CustomScanner{})
-			So(dbToStack(resEmpty, sliceEmpty), ShouldBeEmpty)
-			So(dbToStack(res, slice), ShouldResemble, slice)
-		})
-	})
+func newMockStore() *Store {
+	artisans := []Artisan{
+		Artisan{ID: "id1", Label: "artisan1"},
+		Artisan{ID: "id2", Label: "artisan2"},
+	}
+	return &Store{Artisans: artisans}
 }
 
 func fileExists(filename string) bool {
 	_, err := os.Stat(filename)
 	return !os.IsNotExist(err)
-}
-
-func dbToStack(json string, out []storage.Stack) []storage.Stack {
-	c := converter{}
-	scanner, ok := c.FromDb(&out)
-	if !ok {
-		return nil
-	}
-	if err := scanner.Binder(&json, &out); err != nil {
-		return nil
-	}
-	return out
-}
-
-func dbToType(target interface{}) gorp.CustomScanner {
-	c := converter{}
-	scanner, _ := c.FromDb(target)
-	return scanner
-}
-
-func stackToDb(s []storage.Stack) string {
-	c := converter{}
-	json, err := c.ToDb(s)
-	if err != nil {
-		return err.Error()
-	}
-	return json.(string)
-}
-
-func typeToDb(val interface{}) interface{} {
-	c := converter{}
-	ret, _ := c.ToDb(val)
-	return ret
 }
