6b96514217889f4b2cab936b14867c34efc25637
Martin Chaine <chaine.martin@gmail.com>
2015-01-26 00:01:28 +0100

    More complete tests

--- store/reserve_test.go
+++ store/reserve_test.go
@@ -4,22 +4,30 @@ import (
 	"os"
 	"testing"
 
+	"github.com/casimir/doable"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestStoreSaveAndLoad(t *testing.T) {
 	dbDir := os.TempDir() + "/storekeeper_test/"
 	name := "test"
+	expected := newMockStore()
 	r1 := NewReserve(dbDir, name)
 	r2 := NewReserve(dbDir, name)
 
-	Convey("A Store should be savable and loadable.", t, func() {
+	Convey("A Store could be saved and loaded.", t, func() {
 		So(r1.Save(newMockStore()), ShouldBeNil)
 		So(fileExists(r1.path), ShouldBeTrue)
-		So(r2.Load(), ShouldResemble, newMockStore())
+
+		got := r2.Load()
+		So(got.Artisans, ShouldResemble, expected.Artisans)
+		So(got.Book, ShouldResemble, expected.Book)
+		So(got.Catalog, ShouldResemble, expected.Catalog)
+
 		r1.Delete()
 		So(fileExists(r1.path), ShouldBeFalse)
 	})
+
 	Convey("Reserve should handle invalid states", t, func() {
 		os.RemoveAll(dbDir)
 		So(r1.Save(newMockStore()), ShouldNotBeNil)
@@ -32,7 +40,22 @@ func newMockStore() *Store {
 		&Artisan{ID: "id1", Label: "artisan1"},
 		&Artisan{ID: "id2", Label: "artisan2"},
 	}
-	return &Store{Artisans: artisans}
+	s := NewStore()
+	s.Artisans = artisans
+	s.Catalog.Add(&mockItem{ID: "a", Name: "ab"})
+	return s
+}
+
+type mockItem struct {
+	ID   string
+	Name string
+}
+
+func (i mockItem) Match(o doable.Item) bool {
+	return false
+}
+func (i mockItem) UID() string {
+	return i.ID
 }
 
 func fileExists(filename string) bool {
